2a2015581b993981f6c0e20cfc2ac3231f989c7b
moyen75 <moyenislam75@gmail.com>
2023-08-19 18:58:16 +0600

    remove next-auth related things

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,6 +1,5 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import NextAuthSessionProvider from './providers/sessionProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,9 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NextAuthSessionProvider>
-          {children}
-        </NextAuthSessionProvider>
+        {children}
       </body>
     </html>
   )
