67e5f82cd1b787d099b5c33c392a2654d40ace4b
moyen75 <moyenislam75@gmail.com>
2023-09-04 21:54:54 +0600

    refactor banner

--- frontend/components/Banner.jsx
+++ frontend/components/Banner.jsx
@@ -7,7 +7,8 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 
-import { Autoplay, Pagination} from 'swiper/modules';
+
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
 const Banner = () => {
     return (
@@ -15,23 +16,24 @@ const Banner = () => {
             <Swiper
                 spaceBetween={30}
                 autoplay={{
-                    delay: 2500,
+                    delay: 3500,
                     disableOnInteraction: false,
                 }}
                 pagination={{
                     clickable: true,
                 }}
-                modules={[Autoplay, Pagination]}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
                 className="mySwiper"
             >
                 <SwiperSlide>
                     <img src="/images/hero/1.jpg" alt="" />
                 </SwiperSlide>
                 <SwiperSlide>
-                <img src="/images/hero/2.jpg" alt="" />
+                    <img src="/images/hero/2.jpg" alt="" />
                 </SwiperSlide>
                 <SwiperSlide>
-                <img src="/images/hero/3.jpg" alt="" />
+                    <img src="/images/hero/3.jpg" alt="" />
                 </SwiperSlide>
             </Swiper>
         </>
